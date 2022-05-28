545e722d68cd54097b1973f50ce1cb8b86699a76
Jestyn Apuya <Jestyn@Cecilles-MBP.lan>
2022-05-27 20:03:58 -0700

    Styled card components

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -6,6 +6,9 @@ import { car } from '../types'
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
+const secondaryColor = '#95c93d';
+const cardColor = '#262b36';
+
 export default function CarCard ({car, car_model, id, car_model_year, car_vin, car_color, price, availability}: car){
 
 
@@ -19,16 +22,18 @@ export default function CarCard ({car, car_model, id, car_model_year, car_vin, c
                 uri: `https://picsum.photos/${Math.round(windowWidth * .9)}/${Math.round(windowHeight/5)}`
 
             }} 
-            style = {styles.image}/>
+            style = {styles.image}
+            />
 
             <View style = {styles.description}>
-                <View>
-                    <Text>{car_model}</Text>
-                    <Text>{car}</Text> 
+                <View style = {{backgroundColor: cardColor}}>
+                    <Text style = {styles.model}>{car_model}</Text>
+                    <Text style = {styles.make}>{car_model_year} {car}, {car_color}</Text> 
                 </View>
 
-                <View>
-                     <Text>${parseInt(price.slice(1))}/day</Text>
+                <View style = {{backgroundColor: cardColor}}>
+                     <Text style = {styles.price}>${parseInt(price.slice(1))}/day</Text>
+
                 </View>
 
             </View>
@@ -40,16 +45,56 @@ export default function CarCard ({car, car_model, id, car_model_year, car_vin, c
 
 const styles = StyleSheet.create({
     container: {
+        backgroundColor: cardColor,
+        borderColor: 'black',
+        borderWidth: 1,
+        borderRadius: 10,
         
-        width: '100%'
+        height: windowHeight / 3,
+        width: '100%',
+        marginBottom: 20,
+        
+        shadowColor: 'black',
+        shadowRadius: 5,
+        shadowOpacity: 1,
+        shadowOffset: {width: -1, height: 0}
+
     },
     image : {
-        height: windowHeight / 5,
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10,
+        height: '70%',
         width: windowWidth * .9
     },
     description : {
+        backgroundColor: cardColor,
+
+        borderBottomLeftRadius: 10,
+        borderBottomRightRadius: 10,
+        height: '30%',
+        padding: 10,
         flexDirection: 'row',
+        alignItems: 'flex-end',
         justifyContent: 'space-between'
+    },
+    make : {
+        paddingBottom : '2%',
+        fontSize: '15%',
+        color: 'white',
+        backgroundColor: cardColor
+    },
+    model : {
+        fontSize: '30%',
+        fontWeight: 'bold',
+        color: 'white',
+        backgroundColor: cardColor
+    },
+    price : {
+        paddingBottom: '2%',
+        paddingTop: '10%',
+        fontWeight: 'bold',
+        color: secondaryColor,
+        backgroundColor: cardColor
     }
 
 
